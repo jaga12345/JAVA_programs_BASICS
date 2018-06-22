e76689c15fc91803032e7e7b5310964f2d6e20f1
jaga12345 <jagannath.prasad094@gmail.com>
2018-06-22 17:02:44 +0530

    latest

--- src/basics/Armstrong_Number.java
+++ src/basics/Armstrong_Number.java
@@ -1,21 +1,27 @@
 package basics;
 
+import java.util.Scanner;
+
 public class Armstrong_Number {
 	
 	public static void main(String[] args) throws Throwable{
 		
-		int num=145;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number you want to test");
+		int num=sc.nextInt();
+		
+		//int num=153;
 		int sum=0;
+		int temp=num;
 		
-		
-		while(num!=0) {
+		while(num!=0) {  //while (num>0)
 			
 			int rem=num%10;
 			num=num/10;
 			sum=sum+(rem*rem*rem);
 		}
 		
-		if(sum==num) {
+		if(sum==temp) {
 			
 			System.out.println("its an armstrong number");
 			
