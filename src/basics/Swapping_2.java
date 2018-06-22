b38e14889edf4795823094d0a1e900fbcd29851d
jaga12345 <jagannath.prasad094@gmail.com>
2018-06-22 18:23:29 +0530

    latest

---
+++ src/basics/Swapping_2.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Swapping_2 {
+    
+	public static void main(String[] args) {
+		
+		int temp;
+		int a=10;
+		int b=20;
+		System.out.println("Before Swapping \na = "+a+"\nb = "+b);
+		
+		temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("after  swapping \na= "+a+"\nb = "+b);
+		
+	}
+}
